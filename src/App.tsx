db07715704d263f2fe09d03177f3741921ccb720
kenji <kenjix12x22@gmail.com>
2021-05-07 00:33:43 +0900

    @types/react-router-domインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  return 
-    
+  return (
+    <BrowserRouter>
+
+    </BrowserRouter>
+  )
        
 }
 
---
+++ src/router/Router.tsx
@@ -0,0 +1,7 @@
+import  { memo, VFC} from 'react';
+
+export const Router: VFC = memo(() => {
+  return(
+    
+  )
+});
