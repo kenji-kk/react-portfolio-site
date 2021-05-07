500a73cf4f5848b9d75bbe61249f46b2c8fcadbc
kenji <kenjix12x22@gmail.com>
2021-05-07 17:03:25 +0900

    モバイルバージョンのtopページで文章のアニメーション作成。

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,8 +1,15 @@
 import { memo,VFC } from 'react';
-
+import './Pages.css'
 
 export const Top: VFC = memo(() => {
   return(
-    <p>こんにちは</p>
+    <div className="topBody">
+      <div className="topWrap">
+        <p className="topWelcom">WELCOM</p>
+        <p className="topTo">TO</p>
+        <p className="topKenji">KENJI'S</p>
+        <p className="topPortfolio">PORTFOLIOSITE</p>
+      </div>
+    </div>
   )
 });
