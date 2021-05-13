71957e0420540507606703311035465366da8ae3
kenji <kenjix12x22@gmail.com>
2021-05-14 01:36:38 +0900

    aboutpage修正

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -13,14 +13,15 @@ export const About: VFC = memo(() => {
         <p>2021年4月にIT企業に就職</p>
         <p>出身：愛知</p>
         <p>スポーツ：中学、高校でバスケ</p>
-        <p>趣味：音楽鑑賞、洋画、漫画、アニメ、ゲーム、、旅行、プログラミング</p>
+        <p>趣味：音楽鑑賞、洋画、漫画、アニメ、ゲーム、旅行、プログラミング</p>
         <p>性格：負けず嫌い</p>
-        <p>好きな食べ物；ステーキ、ハンバーグ、焼肉、刺身、カレー</p>
+        <p>好きな食べ物:ステーキ、ハンバーグ、焼肉、刺身、カレー</p>
         <img src="../../../../../images/me.jpg" alt="" className="aboutMyImage"/>
       </div>
       <div className="aboutGitFormWrap"> 
         <div className="aboutGithubWrap">
           <h2>GitHub contribution</h2>
+          <p><a href="https://github.com/kenji-kk">GitHubアカウントハこちら</a></p>
           <img src="https://ghchart.rshah.org/kenji-kk" alt="" className="aboutGithubImg"/>
           </div>
         <div className="aboutFormWrap">
