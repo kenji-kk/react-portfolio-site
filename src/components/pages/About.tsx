513a0d55353ec41d6950d52719797b3e6d9da211
kenji <kenjix12x22@gmail.com>
2021-05-16 17:46:46 +0900

    skillpage完了

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -6,47 +6,49 @@ export const About: VFC = memo(() => {
   return (
     <>
     <div className="aboutImage"></div>
-    <div className="aboutWrap">
-      <div className="aboutIntro">
-        <h2>自己紹介</h2>
-        <p>2021年3月に四年生大学卒業</p>
-        <p>2021年4月にIT企業に就職</p>
-        <p>出身：愛知</p>
-        <p>スポーツ：中学、高校でバスケ</p>
-        <p>趣味：音楽鑑賞、洋画、漫画、アニメ、ゲーム、旅行、プログラミング</p>
-        <p>性格：負けず嫌い</p>
-        <p>好きな食べ物:ステーキ、ハンバーグ、焼肉、刺身、カレー</p>
-        <img src="../../../../../images/me.jpg" alt="" className="aboutMyImage"/>
-      </div>
-      <div className="aboutGitFormWrap"> 
-        <div className="aboutGithubWrap">
-          <h2>GitHub contribution</h2>
-          <p><a href="https://github.com/kenji-kk">GitHubアカウントはこちら</a></p>
-          <img src="https://ghchart.rshah.org/kenji-kk" alt="" className="aboutGithubImg"/>
+    <div className="aboutAllWrap">
+      <div className="aboutWrap">
+        <div className="aboutIntro">
+          <h2>自己紹介</h2>
+          <p>2021年3月に四年生大学卒業</p>
+          <p>2021年4月にIT企業に就職</p>
+          <p>出身：愛知</p>
+          <p>スポーツ：中学、高校でバスケ</p>
+          <p>趣味：音楽鑑賞、洋画、漫画、アニメ、ゲーム、旅行、プログラミング</p>
+          <p>性格：負けず嫌い</p>
+          <p>好きな食べ物:ステーキ、ハンバーグ、焼肉、刺身、カレー</p>
+          <img src="../../../../../images/me.jpg" alt="" className="aboutMyImage"/>
+        </div>
+        <div className="aboutGitFormWrap"> 
+          <div className="aboutGithubWrap">
+            <h2>GitHub contribution</h2>
+            <p><a href="https://github.com/kenji-kk">GitHubアカウントはこちら</a></p>
+            <img src="https://ghchart.rshah.org/kenji-kk" alt="" className="aboutGithubImg"/>
+            </div>
+          <div className="aboutFormWrap">
+            <form>
+              <h2>コンタクト(実装中)</h2>
+              <label>
+                <p>お名前:</p>
+                <input type="text" name="name" className="aboutName"/>
+              </label>
+              <label>
+                <p>メールアドレス:</p>
+                <input type="text" name="name" className="aboutMail"/>
+              </label>
+              <label >
+                <p>お問い合わせ：</p>
+                <textarea  name="name" className="aboutQue"/>
+              </label >
+              <button>送信</button>
+            </form>
           </div>
-        <div className="aboutFormWrap">
-          <form>
-            <h2>コンタクト(実装中)</h2>
-            <label>
-              <p>お名前:</p>
-              <input type="text" name="name" className="aboutName"/>
-            </label>
-            <label>
-              <p>メールアドレス:</p>
-              <input type="text" name="name" className="aboutMail"/>
-            </label>
-            <label >
-              <p>お問い合わせ：</p>
-              <textarea  name="name" className="aboutQue"/>
-            </label >
-            <button>送信</button>
-          </form>
         </div>
       </div>
-    </div>
-    <div className="aboutMessage"> 
-        <h2>私がエンジニアになった理由</h2>
-        <p>coming soon</p>
+      <div className="aboutMessage"> 
+          <h2>私がエンジニアになった理由</h2>
+          <p>coming soon</p>
+      </div>
     </div>
     </>
   )
--- src/components/pages/Skill.tsx
+++ src/components/pages/Skill.tsx
@@ -9,35 +9,53 @@ export const Skill: VFC = memo(() => {
     <div className="skillWrap">
       <div className="skillLeftWrap">
           <div>
-            <img src="../../../../../images/html-5.svg" alt="" />
+            <div>
+             <img src="../../../../../images/html-5.svg" alt="" className="skillHtml"/>
+            </div>
             <p>これまで数々の作品で扱ってきたので、複雑なwebサイトの枠組みを作ることが出来ます。</p>
           </div>
           <div>
-            <img src="../../../../../images/css-3.svg" alt="" />
+            <div>
+              <img src="../../../../../images/css-3.svg" alt="" className="skillCss"/>
+            </div>
             <p>基本的な装飾に加えて、アニメーションなどの複雑なデザインをすることも出来ます。</p>
           </div>
           <div>
-            <img src="../../../../../images/javascript.svg" alt="" />
+            <div>
+              <img src="../../../../../images/javascript.svg" alt="" className="skillJava"/>
+            </div>
             <p>基本的な文法に加えてES６で加えれらた構文も扱うことができます。またDOM操作によってサイトに動きをつけることも可能です。</p>
           </div>
           <div>
-            <img src="../../../../../images/react.svg" alt="" />
+            <div>
+              <img src="../../../../../images/react.svg" alt="" className="skillReact"/>
+            </div>
             <p>ReactによってSPAを作ることができます。本ポートフォリオサイトでも使われています。</p>
           </div>
       </div>
       <div className="skillRightWrap"> 
          <div>
-           <img src="../../../../../images/typescript-icon.svg" alt="" />
+           <div>
+            <img src="../../../../../images/typescript-icon.svg" alt="" className="skillType"/>
+           </div>
            <p>typescriptによって型を宣言するのでバグが起こりにくくなります。本ポートフォリオサイトでも使われています。</p>
           </div>
-            <img src="../../../../../images/ruby.svg" alt="" />
-            <p>Railsを扱うために基本的な文法は押さえています。</p>
           <div>
-            <img src="../../../../../images/rails.svg" alt="" />
+            <div>
+             <img src="../../../../../images/ruby.svg" alt="" className="skillRuby"/>
+            </div>
+            <p>Railsを扱うために基本的な文法は押さえています。</p>
+          </div>
+          <div>
+            <div>
+              <img src="../../../../../images/rails.svg" alt="" className="skillRails"/>
+            </div>
             <p>RailsによってWEBアプリを作ることができます。</p>
           </div>
           <div>
-            <img src="../../../../../images/python.svg" alt="" />
+            <div>
+              <img src="../../../../../images/python.svg" alt="" className="skillPython"/>
+            </div>
             <p>pythonでアルゴリズムの勉強をしています。</p>
           </div>
       </div>
